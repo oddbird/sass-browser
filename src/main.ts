090d5a07e6ac294397f45590800098f9d44dc59b
Ed Rivas <ed@oddbird.net>
2023-02-18 06:17:11 +0000

    First working implementation

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,25 @@
 const sass = require('../build/npm/sass.default.dart.js')
 
-window.sass = sass
+const compiled = sass.compile('style.sass', {
+    importers: [{
+        canonicalize(url: string): URL {
+            return new URL(url, window.location.toString())
+        },
+        load(canonicalUrl: URL) {
+            const request = new XMLHttpRequest();
+            request.onreadystatechange = function() {
+                if (this.readyState === 4 && this.status !== 200)
+                throw new Error(`Failed to fetch ${canonicalUrl}: ${this.status} (${this.statusText})`);
+            };
+            request.open("GET", canonicalUrl, false);
+            request.send();
+            return {
+                contents: request.responseText,
+                syntax: canonicalUrl.pathname.endsWith('.scss') ? 'scss' : 'sass'
+            };
+        }
+    }]
+})
 
-sass.compile('style.scss')
+console.log(compiled)
+document.head.insertAdjacentHTML('beforeend', `<style>${compiled.css}</style>`)
