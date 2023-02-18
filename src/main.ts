74a4aacd3447f97b13d127b3b7f37ea0bc3e67bf
Ed Rivas <ed@oddbird.net>
2023-02-18 08:17:07 +0000

    Compile from string as well

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,37 @@
 const sass = require('../build/npm/sass.default.dart.js')
 
-const compiled = sass.compile('style.sass', {
-    importers: [{
-        canonicalize(url: string): URL {
-            return new URL(url, window.location.toString())
-        },
-        load(canonicalUrl: URL) {
-            const request = new XMLHttpRequest();
-            request.onreadystatechange = function() {
-                if (this.readyState === 4 && this.status !== 200)
-                throw new Error(`Failed to fetch ${canonicalUrl}: ${this.status} (${this.statusText})`);
-            };
-            request.open("GET", canonicalUrl, false);
-            request.send();
-            return {
-                contents: request.responseText,
-                syntax: canonicalUrl.pathname.endsWith('.scss') ? 'scss' : 'sass'
-            };
-        }
-    }]
+window.sass = sass
+
+// Configure importers
+const importers = [{
+    canonicalize(url: string): URL {
+        return new URL(url, window.location.toString())
+    },
+    load(canonicalUrl: URL) {
+        const request = new XMLHttpRequest();
+        request.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status !== 200)
+            throw new Error(`Failed to fetch ${canonicalUrl}: ${this.status} (${this.statusText})`);
+        };
+        request.open("GET", canonicalUrl, false);
+        request.send();
+        console.log(`Importing ${canonicalUrl}`)
+        return {
+            contents: request.responseText,
+            syntax: canonicalUrl.pathname.endsWith('.sass') ? 'indented' : 'scss'
+        };
+    }
+}]
+
+// Compile styles from the textarea
+const input = document.querySelector('#style-input') as HTMLTextAreaElement
+const output = document.querySelector('#style-output') as HTMLTextAreaElement
+const syntaxBox = document.querySelector('#syntax') as HTMLSelectElement
+document.querySelector('[type=button]')?.addEventListener('click', () => {
+    const fromString = sass.compileString(input?.value, {importers, syntax: syntaxBox?.value})
+    output.value = fromString.css
 })
 
-console.log(compiled)
+// Load styles from the server
+const compiled = sass.compile('style.scss', { importers })
 document.head.insertAdjacentHTML('beforeend', `<style>${compiled.css}</style>`)
