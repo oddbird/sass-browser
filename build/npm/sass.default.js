3a0d2012b1942ae1ebc759d135a2e31a63d7fb5b
Jonny Gerig Meyer <jonny@oddbird.net>
2023-05-30 10:59:45 -0400

    Update Dart Sass.

--- build/npm/sass.default.js
+++ build/npm/sass.default.js
@@ -1,18 +1,32 @@
-import * as util from "util"
 import * as immutable from "immutable"
-import * as fs from "fs"
-import * as stream from "stream"
-import _cliPkgModule from "./sass.dart.js";
+import "./sass.dart.js";
 
-// Bundlers may try to resolve this file eagerly, in which case
-// sass.dart.js won't be able to accurately detect that it's being imported
-// rather than required. Fortunately, in that case, exports will be defiend and
-// we can load the library that way instead.
-const _cliPkgLibrary =
-    'load' in _cliPkgModule ? _cliPkgModule : window._cliPkgExports;
-delete window._cliPkgExports;
+const _cliPkgLibrary = globalThis._cliPkgExports.pop();
+if (globalThis._cliPkgExports.length === 0) delete globalThis._cliPkgExports;
 const _cliPkgExports = {};
-_cliPkgLibrary.load({util, immutable, fs, stream}, _cliPkgExports);
+_cliPkgLibrary.load({immutable}, _cliPkgExports);
 
+export const compile = _cliPkgExports.compile;
+export const compileAsync = _cliPkgExports.compileAsync;
 export const compileString = _cliPkgExports.compileString;
 export const compileStringAsync = _cliPkgExports.compileStringAsync;
+export const Logger = _cliPkgExports.Logger;
+export const SassArgumentList = _cliPkgExports.SassArgumentList;
+export const SassBoolean = _cliPkgExports.SassBoolean;
+export const SassColor = _cliPkgExports.SassColor;
+export const SassFunction = _cliPkgExports.SassFunction;
+export const SassList = _cliPkgExports.SassList;
+export const SassMap = _cliPkgExports.SassMap;
+export const SassNumber = _cliPkgExports.SassNumber;
+export const SassString = _cliPkgExports.SassString;
+export const Value = _cliPkgExports.Value;
+export const CustomFunction = _cliPkgExports.CustomFunction;
+export const ListSeparator = _cliPkgExports.ListSeparator;
+export const sassFalse = _cliPkgExports.sassFalse;
+export const sassNull = _cliPkgExports.sassNull;
+export const sassTrue = _cliPkgExports.sassTrue;
+export const Exception = _cliPkgExports.Exception;
+export const PromiseOr = _cliPkgExports.PromiseOr;
+export const info = _cliPkgExports.info;
+export const render = _cliPkgExports.render;
+export const renderSync = _cliPkgExports.renderSync;
